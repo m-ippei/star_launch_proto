fb3c7ef501276e28aff5ebafc18fef737809bbd1
森田　一平 <31976878+m-ippei@users.noreply.github.com>
2019-04-27 20:55:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+const app = new PIXI.Application({ antialias: true });
 document.body.appendChild(app.view);
 
-const basicText = new PIXI.Text('Basic text in pixi');
-basicText.x = 50;
-basicText.y = 100;
+const graphics = new PIXI.Graphics();
 
-app.stage.addChild(basicText);
+// Circle
+graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+graphics.beginFill(0xDE3249, 1);
+graphics.drawCircle(100, 250, 50);
+graphics.endFill();
 
-const style = new PIXI.TextStyle({
-    fontFamily: 'Arial',
-    fontSize: 36,
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    fill: ['#ffffff', '#00ff99'], // gradient
-    stroke: '#4a1850',
-    strokeThickness: 5,
-    dropShadow: true,
-    dropShadowColor: '#000000',
-    dropShadowBlur: 4,
-    dropShadowAngle: Math.PI / 6,
-    dropShadowDistance: 6,
-    wordWrap: true,
-    wordWrapWidth: 440,
-});
-
-const richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
-richText.x = 50;
-richText.y = 250;
-
-app.stage.addChild(richText);
+app.stage.addChild(graphics);
