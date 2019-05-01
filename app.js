cb18f1b2b9f29c6c28e260cd3eefa76f8b671a06
森田一平 <ippei0923@gmail.com>
2019-05-01 20:38:33 +0900

    support PixiJS v5

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
+const sprites = {};
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight;
+const CENTER = {};
+CENTER.x = WIDTH / 2;
+CENTER.y = HEIGHT / 2;
+
 const app = new PIXI.Application({
-    width:window.innerWidth,
-    height:window.innerHeight,
+    width:WIDTH,
+    height:HEIGHT,
     backgroundColor:0xffffe0
 });
 
 app.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
-app.renderer.autoResize = true;
+app.renderer.autoDensity = true;
 app.renderer.resize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(app.view);
 
+PIXI.Loader.shared.add("images/orbs.json").load(setup);
+
+function setup() {
+    let sheet = PIXI.Loader.shared.resources["images/orbs.json"].spritesheet;
+    sprites.red = new PIXI.Sprite(sheet.textures["red.png"]);
+    sprites.red.position.set(CENTER.x,CENTER.y);
+    sprites.red.anchor.set(0.5,0.5);
+    app.stage.addChild(sprites.red);
+}
+
 /*
 
-// Scale mode for all textures, will retain pixelation
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+app.loader.add("orbs","images/orbs.json").load((loader,resources)=>{
+    let sheet = resources["images/orbs.json"]
+    sprites.red = new PIXI.Sprite(sheet.textures["red.png"])
+    sprites.red.position.set(CENTER.x,CENTER.y);
+    app.stage.addChild(sprites.red);
+    
+    const orbs = new PIXI.Sprite(resources.orbs.texture);
+    orbs.x = CENTER.x;
+    orbs.y = CENTER.y;
 
-const sprite = PIXI.Sprite.from('images/red.png');
-
-// Set the initial position
-sprite.anchor.set(0.5);
-sprite.x = app.screen.width / 2;
-sprite.y = app.screen.height / 2;
-
-// Opt-in to interactivity
-sprite.interactive = true;
-
-// Shows hand cursor
-sprite.buttonMode = true;
-
-// Pointers normalize touch and mouse
-sprite.on('pointerdown', onClick);
-
-// Alternatively, use the mouse & touch events:
-// sprite.on('click', onClick); // mouse-only
-// sprite.on('tap', onClick); // touch-only
-
-app.stage.addChild(sprite);
-
-function onClick() {
-    sprite.scale.x *= 1.25;
-    sprite.scale.y *= 1.25;
-}
+    app.stage.addChild(orbs);
+    
+})
 
 */
