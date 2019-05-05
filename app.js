663ae98fc149482eda34274b47bd6d5f953175bc
m-ippei <ippei0923@gmail.com>
2019-05-05 15:27:49 +0900

    add develop file

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
-const sprites = {};
-const WIDTH = window.innerWidth
-const HEIGHT = window.innerHeight;
-const CENTER = {};
-CENTER.x = WIDTH / 2;
-CENTER.y = HEIGHT / 2;
+class StarBoard{
+    constructor(){
+        this._3x5Array = new Array(3).fill(new Array(5).fill(0));
+        this._2x5Array = new Array(2).fill(new Array(5).fill(0));
+        this.sprites = this._3x5Array.map((arr)=>arr.slice());
+        this.colors = this._3x5Array.map((arr)=>arr.slice());
+        this.BoardCenterX = 0;
+        this.BoardCenterY = 0;
+        this.InnerCircleRad = [
+            1.5707963267948966,
+            0.3141592653589793,
+            5.340707511102648,
+            4.084070449666731,
+            2.827433388230814
+        ];
+        this.OuterPositionCircleRad = [
+            [2.6179938779914944,0.5235987755982988],
+            [1.3613568165555772,5.550147021341967],
+            [0.10471975511965977,4.293509959906051],
+            [5.1312680008633285,3.036872898470133],
+            [3.8746309394274117,1.780235837034216]
+        ];
+        this.InnerCircleCenterPositionX = 0;
+        this.InnerCircleCenterPositionY = 0;
+        this.OrbPositions = this._3x5Array.map((arr)=>arr.slice());
+        this.w_Width = window.innerWidth;
+        this.w_Height = window.innerHeight;
 
-const app = new PIXI.Application({
-    width:WIDTH,
-    height:HEIGHT,
-    backgroundColor:0xffffe0
-});
+        this.app = new PIXI.Application({
+            width:this.w_Width,
+            height:this.w_Height,
+            backgroundColor:0xffffe0
+        });
 
-let message = new PIXI.Text(WIDTH.toString()+"  "+HEIGHT.toString());
-app.stage.addChild(message);
+        this.app.view.style.position = "absolute";
+        this.app.renderer.view.style.display = "block";
+        this.app.renderer.autoDensity = true;
+        this.app.resize(this.w_Width,this.w_Height);
 
-message.position.set(100,100);
+        document.body.appendChild(this.app.view);
 
-app.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoDensity = true;
-app.renderer.resize(window.innerWidth,window.innerHeight);
-
-document.body.appendChild(app.view);
-
-PIXI.Loader.shared.add("images/orbs.json").load(setup);
-
-function setup() {
-    let sheet = PIXI.Loader.shared.resources["images/orbs.json"].spritesheet;
-    sprites.red = new PIXI.Sprite(sheet.textures["red.png"]);
-    sprites.red.position.set(CENTER.x,CENTER.y);
-    sprites.red.anchor.set(0.5,0.5);
-    app.stage.addChild(sprites.red);
+        this.sheet = null;
+        
+        
+    }
+    
+    Initilize() {
+        PIXI.Loader.shared.add("images/orbs.json").load(this.Initilize);
+        this.sheet = PIXI.Loader.shared.resources["images/orbs.json"].spritesheet;
+        this._red = new PIXI.Sprite(sheet.textures["red.png"]);
+        this._red.position.set(100,100);
+        this.sprites.reduce.anchor.set(0.5,0.5);
+        this.app.stage.addChild(this._red);
+    }
 }
 
-
+let sb = new StarBoard();
---
+++ doc/memo.js
@@ -0,0 +1,27 @@
+/*let m_sprites = [];
+
+for (let i = 0; i < 3; i++){
+    m_sprites[i] = [];
+    for(let j = 0; j < 5; j++){
+        m_sprites[i][j] = 0;
+    }
+}
+
+console.log(m_sprites);
+
+*/
+
+let colors = new Array(3).fill(new Array(5).fill(0))
+console.log(colors);
+
+let colors = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+]
+
+a.forEach((v)=>{
+    v.forEach((v2)=>{
+        console.log(v2)
+    })
+})
